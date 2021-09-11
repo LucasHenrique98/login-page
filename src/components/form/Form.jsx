c32dff18ac673887554063979cf7db1b3b4530f4
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-09-11 17:46:51 -0300

    creates a yup schema to the form

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,22 +1,53 @@
 import React from 'react';
-import Input from './input/Input.jsx';
 import LoginButton from './logInButton/LoginButton.jsx';
+import { useForm } from 'react-hook-form';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 import './style.scss';
 
+const loginFormSchema = yup.object().shape({
+  email: yup.string().email().required(),
+  password: yup.string().required(),
+});
+
 export default function Form() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginFormSchema) });
+
+  const handleFormSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <form className="form-container">
+    <form onSubmit={handleSubmit(handleFormSubmit)} className="form-container">
       <h1>Hey, welcome!</h1>
 
       <label className="input-container" htmlFor="email">
         <p className="label-title">E-mail</p>
-        <Input type="email" name="email" placeholder="Insira seu e-mail" />
+        <input
+          className="form-input"
+          {...register('email')}
+          type="email"
+          name="email"
+          placeholder="Insira seu e-mail"
+        />
       </label>
+      {errors.email && <p className="input-error-message">{errors.email.message}</p>}
 
       <label className="input-container" htmlFor="password">
         <p className="label-title">Password</p>
-        <Input type="password" name="password" placeholder="Insira sua senha" />
+        <input
+          className="form-input"
+          {...register('password')}
+          type="password"
+          name="password"
+          placeholder="Insira sua senha"
+        />
       </label>
+      {errors.password && <p className="input-error-message">{errors.password.message}</p>}
 
       <LoginButton />
     </form>
