9999f2cfb24e8975ab0106cfc399dbc2cdba2752
LucasHenrique98 <henrique.lucas30@gmail.com>
2021-09-11 15:34:07 -0300

    creates a form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import Form from './components/form/Form';
 
-export function App(){
+export function App() {
   return (
     <div>
-      <h1>Welcome Lucas</h1>
+      <Form />
     </div>
   );
 }
---
+++ src/components/form/Form.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.scss';
+
+export default function Form() {
+  return (
+    <div className="form-container">
+      <h1>Olá mundo</h1>
+    </div>
+  );
+}
